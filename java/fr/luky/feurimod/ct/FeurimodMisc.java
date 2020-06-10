2a993ae0bf03ddc57e614258908f6283a8c7863b
baztoul76 <66006344+baztoul76@users.noreply.github.com>
2020-06-10 15:31:31 +0200

    Create FeurimodMisc.java

---
+++ java/fr/luky/feurimod/ct/FeurimodMisc.java
@@ -0,0 +1,17 @@
+package fr.luky.feurimod.ct;
+
+import fr.luky.feurimod.init.ModItems;
+
+import minecraft.net.creativetab.CreativeTabs;
+import minecraft.net.item.ItemStack;
+
+public class FeurimodMisc extends CreativeTabs{
+	public FeurimodMisc(String label){
+		super(label);
+	}
+	
+	@override
+	public ItemStack getTabIconItem(){
+		return new ItemStack(modItems.MOLTEN_FEURIUM);
+	}
+}
