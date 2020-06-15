a8dae20c8f5faffcc2fbc9f35a03ef72624a12d8
baztoul76 <66006344+baztoul76@users.noreply.github.com>
2020-06-15 18:55:52 +0200

    Create BlockOriumBasin.java

---
+++ java/fr/luky/feurimod/blocks/BlockOriumBasin.java
@@ -0,0 +1,8 @@
+package fr.luky.feurimod;
+
+import net.minecraft.Block;
+import net.minecraft.ITileEntityProvider;
+
+public class BlockOriumBasin extends Block implements ITileEntityProvider {
+	// la flemme
+}
